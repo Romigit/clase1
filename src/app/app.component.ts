143a0417f7635e3d5ddc3fd84ecea71efe1f1aa4
unknown <romigit@users.noreply.github.com>
2021-03-25 04:34:06 -0300

    Ejercicio 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'clase2';
+  title : string;
+  edadUno:string;
+  edadDos:string;
+  promedio : number;
+  suma: number;
+  limpiar : string;
+  calculo : number;
+
+  constructor(){
+    this.title = 'clase1';
+    this.edadUno ='';
+    this.edadDos='';
+    this.promedio = 0;
+    this.suma = 0;
+    this.limpiar = '';
+    this.calculo = 0;
+  }
+
+
+  HacerCambios(){
+    this.title='angular 2021, bienvenidos';
+  }
+
+  obtenerPromedio():number{
+    this.promedio=this.obtenerSuma()/2;
+    return this.calculo=this.promedio;
+
+  }
+  obtenerSuma():number{
+    this.suma=parseInt(this.edadUno)+parseInt(this.edadDos);
+    return this.calculo=this.suma;
+  }
+  limpiarCuadrosDeTextos(){
+    this.edadUno ='';
+    this.edadDos='';
+    this.calculo=0;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
+  declarations: [ //son los componentes que yo puedo utilizar
     AppComponent
   ],
-  imports: [
+  imports: [  //son los modulos que yo puedo utilizar. Pueden tener componentes, clases, interfaces, otros modulos. 
+              //si el modulo tiene componentes debe estar declarado arriba para poder usarlos.
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [], //son los servicios, conexión a base
   bootstrap: [AppComponent]
 })
 export class AppModule { }
