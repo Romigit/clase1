27b0e49c4b95093662e243c08df62b2b055c8677
unknown <romigit@users.noreply.github.com>
2021-04-07 23:54:05 -0300

    se modifico src de la carpeta del proyecto server.js para ver si funciona en heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/clase1'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/clase1/index.html'));
