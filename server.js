c427d92b2b5dcdf1b5b5c6fd02a3ef7f6e93e8df
unknown <romigit@users.noreply.github.com>
2021-04-07 22:31:47 -0300

    se modifico scr por src de la carpeta del proyecto server.js para ver si funciona en heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static(__dirname + '/scr'));
+app.use(express.static(__dirname + '/src'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/src/index.html'));
